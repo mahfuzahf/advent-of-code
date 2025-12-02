f1e6995499bf87ae1da52570db20900f8192165a
Mahfuzah <mahfuzahf@outlook.com>
2025-12-02 22:20:43 +0000

    set up files for tomorrow

---
+++ 2025/days/day_3.py
@@ -0,0 +1,30 @@
+# imports
+import re
+import math
+
+# Constants
+INPUT = '2025/resources/day_3_input.txt'
+TEST = '2025/resources/day_3_test.txt'
+
+#############################################################################################################
+
+# get our input
+def get_input(filename):
+
+    # read the file
+    with open (filename, 'r') as f:
+        data = f.read()
+
+    # return data
+    return data
+
+
+#############################################################################################################
+
+input = get_input(TEST)
+print(input)
+
+#############################################################################################################
+
+
+
