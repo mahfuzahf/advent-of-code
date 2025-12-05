95036dbbd1c7f01f285abf5e1a2d127dba1af489
Mahfuzah <mahfuzahf@outlook.com>
2025-12-05 16:02:43 +0000

    prepped for day 6

---
+++ 2025/days/day_6.py
@@ -0,0 +1,41 @@
+# imports
+
+# Constants
+INPUT = '2025/resources/day_6_input.txt'
+TEST = '2025/resources/day_6_test.txt'
+
+#############################################################################################################
+
+def get_input(filename):
+
+    # read the file
+    with open (filename, 'r') as f:
+        data = f.read().splitlines()
+
+    # return data
+    return data
+
+
+#############################################################################################################
+
+def part_1(data):
+    answer = 0
+
+    return answer
+
+
+#############################################################################################################
+
+def part_2(data):
+    answer = 0
+
+    return answer
+
+#############################################################################################################
+
+data = get_input(TEST)
+print(data)
+
+
+print(part_1(data))
+# print(part_2(data))
